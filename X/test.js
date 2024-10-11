543dde31ddb11c628355d6a015ff5b0fcd4d31c2
方块 <57312083+fangkuia@users.noreply.github.com>
2024-10-11 15:13:32 +0900

    Update test.js

--- X/test.js
+++ X/test.js
@@ -1,74 +1,96 @@
-/*
-{
-  "follows": [
-    {"name": "bilibili 大会员", "uid": "233233"}, 
-    {"name": "bilibili 官方账号", "uid": "1"} 
-  ]
-}
-*/
+const cheerio = createCheerio()
+const CryptoJS = createCryptoJS()
+
 let $config = argsify($config_str)
 
 let appConfig = {
     ver: 1,
-    title: 'Bilibili',
+    title: '哔哩哔哩',
     site: 'https://www.bilibili.com',
 }
 
 async function getConfig() {
-  let tabs = []
-  $config.follows.forEach( each => {
-    tabs.push({
-      name: each.name,
-      ext: {
-        uid: each.uid,
-      }
+    let tabs = []
+    $config.follows.forEach((each) => {
+        tabs.push({
+            name: each.name,
+            ext: {
+                code: each.code,
+            },
+        })
     })
-  })
-  return jsonify({
-      ver: 1,
-      title: 'Bilibili',
-      site: 'https://www.bilibili.com',
-      tabs
-  })
+    await initSession()
+    return jsonify({
+        ver: 1,
+        title: '哔哩哔哩',
+        site: 'https://www.bilibili.com',
+        tabs,
+    })
+}
+
+async function initSession() {
+    const url = appConfig.site
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    }
+    const { data } = await $fetch.get(url, {
+        headers,
+    })
+    // 这里可以解析 Bilibili 的相关配置
 }
 
 async function getCards(ext) {
-  ext = argsify(ext)
-  uid = ext.uid
-  const url = `https://api.bilibili.com/x/space/arc/search?mid=${uid}&ps=30&tid=0&pn=1&keyword=&order=pubdate&jsonp=jsonp`
-  const headers = {
-    Origin: 'https://www.bilibili.com',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-  }
-  const { data } = await $fetch.get(url, {
-    headers
-  });
-  $print(`***url: ${url}`)
-  let cards = []
-  data.data.list.vlist.forEach( each => {
-    cards.push({
-      vod_id: each.aid,
-      vod_name: each.title,
-      vod_pic: each.pic,
-      vod_remarks: '',
-      ext: {
-        id: each.aid
-      }
-    })
-  })
+    ext = argsify(ext)
+    let code = ext.code
+    let page = ext.page
+    let cards = []
 
-  return jsonify({
-      list: cards,
-  })
+    const url = `https://api.bilibili.com/x/space/arc/search?mid=${code}&pn=${page}&ps=20`
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Content-Type': 'application/json',
+    }
+
+    const { data } = await $fetch.get(url, { headers })
+    const videos = argsify(data).data.list.vlist
+
+    videos.forEach((item) => {
+        cards.push({
+            vod_id: item.bvid,
+            vod_name: item.title,
+            vod_pic: item.pic,
+            vod_remarks: item.pubdate,
+            ext: {
+                id: item.bvid,
+            },
+        })
+    })
+
+    return jsonify({
+        list: cards,
+    })
+}
+
+async function getChannelId(code) {
+    const url = `https://space.bilibili.com/${code}`
+    const headers = {
+        Origin: 'https://www.bilibili.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    }
+    const response = await $fetch.get(url, { headers })
+    const $ = cheerio.load(response.data)
+    const link = $('link[rel="canonical"]').attr('href')
+    const channelId = link.split('/space/')[1]
+    return channelId
 }
 
 async function getTracks(ext) {
     ext = argsify(ext)
     let tracks = []
     tracks.push({
-      name: '播放',
-      pan: '',
-      ext
+        name: '播放',
+        pan: '',
+        ext,
     })
 
     return jsonify({
@@ -82,25 +104,45 @@ async function getTracks(ext) {
 }
 
 async function getPlayinfo(ext) {
-  ext = argsify(ext)
-  let videoId = ext.id
-  $print(`***id: ${videoId}`)
-  const headers = {
-    'Referer': 'https://www.bilibili.com',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-  }
+    ext = argsify(ext)
+    let videoId = ext.id
+    const url = `https://api.bilibili.com/x/player/playurl?bvid=${videoId}`
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    }
 
-  const { data } = await $fetch.get(`https://api.bilibili.com/x/player/playurl?avid=${videoId}&cid=${videoId}&qn=80`, {
-    headers
-  })
-
-  let playurl = data.data.durl[0].url
-  $print(`***playurl: ${playurl}`)
-  return jsonify({ urls: [playurl] })
+    const { data } = await $fetch.get(url, { headers })
+    let playurl = argsify(data).data.durl[0].url
+    return jsonify({ urls: [playurl] })
 }
 
 async function search(ext) {
+    ext = argsify(ext)
+    let cards = []
+    const text = ext.text
+    const url = `https://api.bilibili.com/x/web-interface/search/type?keyword=${text}&search_type=video`
+    const headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Content-Type': 'application/json',
+    }
+
+    const { data } = await $fetch.get(url, { headers })
+    const videos = argsify(data).data.result
+
+    videos.forEach((item) => {
+        cards.push({
+            vod_id: item.bvid,
+            vod_name: item.title,
+            vod_pic: item.pic,
+            vod_remarks: item.pubdate,
+            ext: {
+                id: item.bvid,
+            },
+        })
+    })
+
     return jsonify({
-        list: [],
+        list: cards,
     })
 }
+
