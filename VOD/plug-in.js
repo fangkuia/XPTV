6db53842311edece2d1fa363844096292abc9675
方块 <57312083+fangkuia@users.noreply.github.com>
2024-10-12 22:12:17 +0900

    Create plug-in.js

---
+++ VOD/plug-in.js
@@ -0,0 +1,10 @@
+{
+    "sites": [
+        { 
+            "name": "Youtube", 
+            "type": 3, 
+            "api": "csp_youtube",
+            "ext": "https://infuseku.xyz/vod/you.js"
+        }
+    ]
+}
