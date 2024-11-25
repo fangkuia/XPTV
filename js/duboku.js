2a077029e0bb1e885c28a9137591525b8a9ee96a
方块 <57312083+fangkuia@users.noreply.github.com>
2024-11-26 01:24:19 +0900

    Update duboku.js

--- js/duboku.js
+++ js/duboku.js
@@ -1,3 +1,4 @@
+//老登
 const cheerio = createCheerio()
 const UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 const headers = {
