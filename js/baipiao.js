8690c1d04d56085657c1e1346c03fc6ab24fb726
方块 <57312083+fangkuia@users.noreply.github.com>
2024-12-30 11:42:57 +0900

    Create baipiao.js

---
+++ js/baipiao.js
@@ -0,0 +1,147 @@
+const cheerio = createCheerio()
+
+const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+
+const appConfig = {
+	ver: 1,
+	title: '白嫖',
+	site: 'https://smdyo.top',
+	tabs: [
+		{
+			name: '電影',
+			ext: {
+				id: 30,
+			},
+		},
+		{
+			name: '劇集',
+			ext: {
+				id: 25,
+			},
+		},
+		{
+			name: '動漫',
+			ext: {
+				id: 20,
+			},
+		},
+	],
+}
+
+async function getConfig() {
+	return jsonify(appConfig)
+}
+
+async function getCards(ext) {
+	
+	ext = argsify(ext)
+	let cards = []
+	let { page = 1, id } = ext
+
+	const url = appConfig.site + `/index.php/vod/show/id/${id}/page/${page}.html`
+	const { data } = await $fetch.get(url, {
+		headers: {
+			'User-Agent': UA,
+		},
+	})
+
+	const $ = cheerio.load(data)
+
+	const videos = $('#main .module-item')
+	videos.each((_, e) => {
+		const href = $(e).find('.module-item-pic a').attr('href')
+		const title = $(e).find('.module-item-pic a').attr('title')
+		const cover = $(e).find('.module-item-pic img').attr('data-src')
+		const remarks = $(e).find('.module-item-text').text()
+		
+		cards.push({
+			vod_id: href,
+			vod_name: title,
+			vod_pic: cover,
+			vod_remarks: remarks, 
+		
+			ext: {
+				url: `${appConfig.site}${href}`,
+			},
+		})
+	})
+
+	return jsonify({
+		list: cards,
+	})
+}
+
+async function getTracks(ext) {
+	ext = argsify(ext)
+	let tracks = []
+	let url = ext.url
+
+	const { data } = await $fetch.get(url, {
+		headers: {
+			'User-Agent': UA,
+		},
+	})
+
+	const $ = cheerio.load(data)
+
+	const playlist = $('.module-player-list .module-row-one')
+	playlist.each((_, e) => {
+		const panShareUrl = $(e).find('.module-row-title p').text()
+		tracks.push({
+			name: '网盘',
+			pan: panShareUrl,
+		})
+	})
+
+	return jsonify({
+		list: [
+			{
+				title: '默认分组',
+				tracks,
+			},
+		],
+	})
+}
+
+async function getPlayinfo(ext) {
+	return jsonify({ urls: [] })
+}
+
+async function search(ext) {
+	ext = argsify(ext)
+	let cards = []
+
+	let text = encodeURIComponent(ext.text)
+	let page = ext.page || 1
+	let url = `${appConfig.site}/index.php/vod/search/page/${page}/wd/${text}.html`
+
+	const { data } = await $fetch.get(url, {
+		headers: {
+			'User-Agent': UA,
+		},
+	})
+
+	const $ = cheerio.load(data)
+
+	const videos = $('#main .module-search-item')
+	videos.each((_, e) => {
+		const href = $(e).find('.video-info-header h3 a').attr('href')
+		const title = $(e).find('.video-info-header h3 a').attr('title')
+		const cover = $(e).find('.module-item-pic img').attr('data-src')
+		const remarks = $(e).find('.video-serial').text()
+		
+		cards.push({
+			vod_id: href,
+			vod_name: title,
+			vod_pic: cover,
+			vod_remarks: remarks,
+			ext: {
+				url: `${appConfig.site}${href}`,
+			},
+		})
+	})
+
+	return jsonify({
+		list: cards,
+	})
+}
