fd89367726beb1ec7c5f80e4b674f9735e9aae8c
方块 <57312083+fangkuia@users.noreply.github.com>
2025-01-07 16:56:03 +0900

    Create seedhub.js

---
+++ js/seedhub.js
@@ -0,0 +1,157 @@
+const cheerio = createCheerio()
+const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)'
+
+const appConfig = {
+	ver: 1,
+	title: 'SeedHub',
+	site: 'https://www.seedhub.cc',
+	tabs: [
+		{
+			name: '首页',
+			ext: {
+				id: '/',
+			},
+		},
+		{
+			name: '电影',
+			ext: {
+				id: '/categories/1/movies/',
+			},
+		},
+		{
+			name: '剧集',
+			ext: {
+				id: '/categories/3/movies/',
+			},
+		},
+		{
+			name: '动漫',
+			ext: {
+				id: '/categories/2/movies/',
+			},
+		}
+		
+	],
+}
+async function getConfig() {
+	return jsonify(appConfig)
+}
+
+async function getCards(ext) {
+	ext = argsify(ext)
+	let cards = []
+	let { page = 1, id } = ext
+	const url =appConfig.site + id + `?page=${page}`
+	const { data } = await $fetch.get(url, {
+    headers: {
+		"User-Agent": UA,
+  	  },
+});
+	
+	const $ = cheerio.load(data)
+	const videos = $('.cover')
+	videos.each((_, e) => {
+	const href = $(e).find('a').attr('href')
+	const title = $(e).find('a img').attr('alt')
+	const cover = $(e).find('a img').attr('src')
+	cards.push({
+			vod_id: href,
+			vod_name: title,
+			vod_pic: cover,
+			vod_remarks: '',
+			ext: {
+				url: `${appConfig.site}${href}`,
+			},
+		})
+	})
+		return jsonify({
+		list: cards,
+	})
+}
+
+async function getTracks(ext) {
+	ext = argsify(ext);
+	let tracks = [];
+	let url = ext.url;
+
+	const { data } = await $fetch.get(url, {
+		headers: {
+			'User-Agent': UA,
+		},
+	});
+	
+	const $ = cheerio.load(data);
+	const playlist = $('.pan-links');
+	
+	if (playlist.length === 0 || playlist.find('li').length === 0) {
+		$utils.toastError('没有网盘资源'); 
+		return jsonify({ list: [] }); 
+	}
+	
+	playlist.each((_, e) => {
+		
+		$(e).find('li a').each((_, link) => {
+			const href = $(link).attr('href');
+			const regex = /redirect_to=(.*?)&movie_title=/;
+			const match = href.match(regex);
+
+			if (match && match[1]) { 
+				tracks.push({
+					name: '网盘',
+					pan: match[1], 
+				});
+			}
+		});
+	});
+	
+	return jsonify({
+		list: [
+			{
+				title: '默认分组', 
+				tracks,
+			},
+		],
+	});
+}
+async function getPlayinfo(ext) {
+	ext = argsify(ext)
+	const url = ext.url
+   	  
+	return jsonify({ urls: [ext.url] })
+}
+
+async function search(ext) {
+	ext = argsify(ext)
+	let cards = []
+
+	let text = encodeURIComponent(ext.text)
+	let page = ext.page || 1
+	let url = `${appConfig.site}/s/${text}/?page=${page}`
+
+	const { data } = await $fetch.get(url, {
+		headers: {
+			'User-Agent': UA,
+		},
+	})
+
+	const $ = cheerio.load(data)
+	const videos = $('.cover')
+	videos.each((_, e) => {
+	const href = $(e).find('a').attr('href')
+	const title = $(e).find('a img').attr('alt')
+	const cover = $(e).find('a img').attr('src')
+	cards.push({
+			vod_id: href,
+			vod_name: title,
+			vod_pic: cover,
+			vod_remarks: '',
+			ext: {
+				url: `${appConfig.site}${href}`,
+			},
+		})
+	})
+
+	return jsonify({
+		list: cards,
+	})
+}
