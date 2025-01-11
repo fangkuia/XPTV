10f719c294a52913522fd86ce09fb1394837a387
方块 <57312083+fangkuia@users.noreply.github.com>
2025-01-11 17:27:03 +0900

    Update tgs.js

--- js/tgs.js
+++ js/tgs.js
@@ -95,7 +95,7 @@ async function getCards(ext) {
                            title = titletext.split('S')[0]
                        }
                    })
-                   title = title.replace(/<b>/, '').replace(/4K.*$/g, '')
+                title = title.replace(/<b>/, '').replace(/4K.*$/g, '').replace('发行时间', '')
 
 
                 $(element)
@@ -221,7 +221,7 @@ async function search(ext) {
                            title = titletext.split('S')[0]
                        }
                    })
-                   title = title.replace(/<b>/, '').replace(/4K.*$/g, '')
+                title = title.replace(/<b>/, '').replace(/4K.*$/g, '').replace('发行时间', '')
 
                 $(element)
                     .find('.tgme_widget_message_text > a')
