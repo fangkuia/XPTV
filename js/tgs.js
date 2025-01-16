a50c5d68c4fa8a704c7f59565027758ea2911d1a
方块 <57312083+fangkuia@users.noreply.github.com>
2025-01-16 15:17:36 +0900

    Update tgs.js

--- js/tgs.js
+++ js/tgs.js
@@ -80,22 +80,25 @@ async function getCards(ext) {
             let remarks = ''
             try {
                 const html = $(element).find('.tgme_widget_message_text').html().replace(/<b[^>]*>|<\/b>|<a[^>]*>|<\/a>|<mark[^>]*>|<\/mark>|<i[^>]*>|<\/i>|<div[^>]*>|<\/div>/g, '').replace(/【[^】]*】/g, '')
-                   html.split('<br>').forEach((e) => {
-                       const titletext = e.trim()
-                       if (/(名称|名字|短剧|资源标题)(：|:)/.test(titletext)) {
-                           title = titletext
-                               .split(/：|:/)[1]
-                               .trim()
-                               .split(/（|\(|\[|(更新?至|全)/)[0]
-                       } else if (/（|\(|\[|(更新?至|全)/.test(titletext)) {
-                           title = titletext.split(/（|\(|\[|(更新?至|全)/)[0]
-                       } else if (/(.+)\s(更新?至|全)/.test(titletext)) {
-                           title = titletext.split(/更新?至|全/)[0]
-                       } else if (/S\d+/.test(titletext)) {
-                           title = titletext.split('S')[0]
-                       }
-                   })
-                title = title.replace(/<b>/, '').replace(/4K.*$/g, '').replace('发行时间', '')
+                html.split('<br>').forEach((e) => {
+                    const titletext = e.trim()
+                    if (/(名称|名字|短剧|资源标题)(：|:)/.test(titletext)) {
+                        title = titletext
+                            .split(/：|:/)[1]
+                            .trim()
+                        //如果第一字符是[则匹配第二个[
+                        if (title.startsWith('[')) {
+                            title = title.split('][')[0].replace('[', '')
+                        } else { title = title.split(/（|\(|\[|(更新?至|全)/)[0] }
+                    } else if (/（|\(|\[|(更新?至|全)/.test(titletext)) {
+                        title = titletext.split(/（|\(|\[|(更新?至|全)/)[0]
+                    } else if (/(.+)\s(更新?至|全)/.test(titletext)) {
+                        title = titletext.split(/更新?至|全/)[0]
+                    } else if (/S\d+/.test(titletext)) {
+                        title = titletext.split('S')[0]
+                    }
+                })
+                title = title.replace(/<b>/, '').replace(/4K.*$/g, '').replace('发行时间', '').replace('描述', '').trim()
 
 
                 $(element)
@@ -111,11 +114,11 @@ async function getCards(ext) {
                     .attr('style')
                     .match(/image\:url\('(.+)'\)/)[1]
                 remarks = hrefs[0].match(/https:\/\/(.+)\/(s|t)\//)[1].replace(/(115\.com)|(anxia\.com)/, '115')
-                .replace(/(pan\.quark\.cn)/, '夸克')
-                .replace(/(drive\.uc\.cn)/, 'UC')
-                .replace(/(www\.aliyundrive\.com)|(www\.alipan\.com)/, '阿里')
-                .replace(/(cloud\.189\.cn)/, '天翼')
-                .trim();
+                    .replace(/(pan\.quark\.cn)/, '夸克')
+                    .replace(/(drive\.uc\.cn)/, 'UC')
+                    .replace(/(www\.aliyundrive\.com)|(www\.alipan\.com)/, '阿里')
+                    .replace(/(cloud\.189\.cn)/, '天翼')
+                    .trim();
             } catch (e) {
                 // $utils.toastError(`${ext.channel}搜索失败`)
             }
@@ -127,6 +130,7 @@ async function getCards(ext) {
                 vod_remarks: remarks,
                 ext: {
                     url: hrefs,
+                    name: title,
                 },
             })
         })
@@ -159,9 +163,10 @@ async function getTracks(ext) {
     ext = argsify(ext)
     let tracks = []
     let urls = ext.url
+    const name = ext.name
     urls.forEach((url) => {
         tracks.push({
-            name: '网盘',
+            name: name,
             pan: url,
         })
     })
@@ -189,73 +194,83 @@ async function search(ext) {
         })
     }
     let text = encodeURIComponent(ext.text)
-
-    for (const channel of $config.channels) {
-        let url = `${appConfig.site}${channel}?q=${text}`
-        const { data } = await $fetch.get(url, {
-            headers: {
-                'User-Agent': UA,
-            },
-        })
-        const $ = cheerio.load(data)
-        if ($('div.tgme_widget_message_bubble').length === 0) continue
-        $('div.tgme_widget_message_bubble').each((_, element) => {
-            let title = ''
-            let hrefs = []
-            let cover = ''
-            let remarks = ''
-            try {
-                const html = $(element).find('.tgme_widget_message_text').html().replace(/<b[^>]*>|<\/b>|<a[^>]*>|<\/a>|<mark[^>]*>|<\/mark>|<i[^>]*>|<\/i>|<div[^>]*>|<\/div>/g, '').replace(/【[^】]*】/g, '')
-                   html.split('<br>').forEach((e) => {
-                       const titletext = e.trim()
-                       if (/(名称|名字|短剧|资源标题)(：|:)/.test(titletext)) {
-                           title = titletext
-                               .split(/：|:/)[1]
-                               .trim()
-                               .split(/（|\(|\[|(更新?至|全)/)[0]
-                       } else if (/（|\(|\[|(更新?至|全)/.test(titletext)) {
-                           title = titletext.split(/（|\(|\[|(更新?至|全)/)[0]
-                       } else if (/(.+)\s(更新?至|全)/.test(titletext)) {
-                           title = titletext.split(/更新?至|全/)[0]
-                       } else if (/S\d+/.test(titletext)) {
-                           title = titletext.split('S')[0]
-                       }
-                   })
-                title = title.replace(/<b>/, '').replace(/4K.*$/g, '').replace('发行时间', '')
-
-                $(element)
-                    .find('.tgme_widget_message_text > a')
-                    .each((_, element) => {
-                        const href = $(element).attr('href')
-                        if (href.match(/https:\/\/(.+)\/(s|t)\/(.+)/)) {
-                            hrefs.push(href)
+    const requests = $config.channels.map(async (channel) => {
+        const url = `${appConfig.site}${channel}?q=${text}`;
+        try {
+            const { data } = await $fetch.get(url, {
+                headers: {
+                    'User-Agent': UA,
+                },
+            });
+            const $ = cheerio.load(data)
+            if ($('div.tgme_widget_message_bubble').length === 0) return
+            $('div.tgme_widget_message_bubble').each((_, element) => {
+                let title = ''
+                let hrefs = []
+                let cover = ''
+                let remarks = ''
+                try {
+                    const html = $(element).find('.tgme_widget_message_text').html().replace(/<b[^>]*>|<\/b>|<a[^>]*>|<\/a>|<mark[^>]*>|<\/mark>|<i[^>]*>|<\/i>|<div[^>]*>|<\/div>/g, '').replace(/【[^】]*】/g, '')
+                    html.split('<br>').forEach((e) => {
+                        const titletext = e.trim()
+                        if (/(名称|名字|短剧|资源标题)(：|:)/.test(titletext)) {
+                            title = titletext
+                                .split(/：|:/)[1]
+                                .trim()
+                            //如果第一字符是[则匹配第二个[
+                            if (title.startsWith('[')) {
+                                title = title.split('][')[0].replace('[', '')
+                            } else { title = title.split(/（|\(|\[|(更新?至|全)/)[0] }
+                        } else if (/（|\(|\[|(更新?至|全)/.test(titletext)) {
+                            title = titletext.split(/（|\(|\[|(更新?至|全)/)[0]
+                        } else if (/(.+)\s(更新?至|全)/.test(titletext)) {
+                            title = titletext.split(/更新?至|全/)[0]
+                        } else if (/S\d+/.test(titletext)) {
+                            title = titletext.split('S')[0]
                         }
                     })
-                cover = $(element)
-                    .find('.tgme_widget_message_photo_wrap')
-                    .attr('style')
-                    .match(/image\:url\('(.+)'\)/)[1]
-                remarks = hrefs[0].match(/https:\/\/(.+)\/(s|t)\//)[1].replace(/(115\.com)|(anxia\.com)/, '115')
-                .replace(/(pan\.quark\.cn)/, '夸克')
-                .replace(/(drive\.uc\.cn)/, 'UC')
-                .replace(/(www\.aliyundrive\.com)|(www\.alipan\.com)/, '阿里')
-                .replace(/(cloud\.189\.cn)/, '天翼')
-                .trim();
-            } catch (e) {
-                $utils.toastError(`${channel}搜索失败`)
-            }
-            if (remarks === '') return
-            cards.push({
-                vod_id: hrefs[0],
-                vod_name: title,
-                vod_pic: cover,
-                vod_remarks: remarks,
-                ext: {
-                    url: hrefs,
-                },
+                    title = title.replace(/<b>/, '').replace(/4K.*$/g, '').replace('发行时间', '').replace('描述', '').trim()
+
+                    $(element)
+                        .find('.tgme_widget_message_text > a')
+                        .each((_, element) => {
+                            const href = $(element).attr('href')
+                            if (href.match(/https:\/\/(.+)\/(s|t)\/(.+)/)) {
+                                hrefs.push(href)
+                            }
+                        })
+                    cover = $(element)
+                        .find('.tgme_widget_message_photo_wrap')
+                        .attr('style')
+                        .match(/image\:url\('(.+)'\)/)[1]
+                    remarks = hrefs[0].match(/https:\/\/(.+)\/(s|t)\//)[1].replace(/(115\.com)|(anxia\.com)/, '115')
+                        .replace(/(pan\.quark\.cn)/, '夸克')
+                        .replace(/(drive\.uc\.cn)/, 'UC')
+                        .replace(/(www\.aliyundrive\.com)|(www\.alipan\.com)/, '阿里')
+                        .replace(/(cloud\.189\.cn)/, '天翼')
+                        .trim();
+                } catch (e) {
+                    //$utils.toastError(`${channel}搜索失败`)
+                }
+                if (remarks === '') return
+                cards.push({
+                    vod_id: hrefs[0],
+                    vod_name: title,
+                    vod_pic: cover,
+                    vod_remarks: remarks,
+                    vod_duration: channel,
+                    ext: {
+                        url: hrefs,
+                        name: title,
+                    },
+                })
             })
-        })
-    }
+        } catch (error) {
+            //
+        }
+
+    });
+    await Promise.all(requests);
     return jsonify({
         list: cards,
     })
