0df0f0c910cb7965c7a709cec7e77ab35afee4dd
方块 <57312083+fangkuia@users.noreply.github.com>
2025-02-14 22:45:37 +0900

    Update wogg.js

--- js/wogg.js
+++ js/wogg.js
@@ -4,7 +4,7 @@ const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/6
 const appConfig = {
 	ver: 1,
 	title: '玩偶哥哥',
-	site: 'https://www.wogg.net',
+	site: 'https://www.wogg.one',
 
 	tabs: [
 		{
