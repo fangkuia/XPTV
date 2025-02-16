e9f98f1787665b3d4373a5ed9fbd118690de19ac
方块 <57312083+fangkuia@users.noreply.github.com>
2025-02-17 02:03:09 +0900

    Update muou.js

--- js/muou.js
+++ js/muou.js
@@ -21,7 +21,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 const appConfig = {
     ver: 1,
     title: '木偶',
-    site: 'https://muou.666291.xyz',
+    site: 'https://91muou.icu',
     // 定義分類
     tabs: [
         // name 為分類名，ext 可以傳入任意參數由 getCards 接收
