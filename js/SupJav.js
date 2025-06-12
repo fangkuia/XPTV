7da45b6a972a5b8badb14f486aede4ed7744ffe7
方块 <57312083+fangkuia@users.noreply.github.com>
2025-06-13 00:51:41 +0900

    Update SupJav.js

--- js/SupJav.js
+++ js/SupJav.js
@@ -1,7 +1,6 @@
 const cheerio = createCheerio()
 
-const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
-
+const UA = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/604.1.14 (KHTML, like Gecko)'
 const appConfig = {
     ver: 1,
     title: 'SupJav',
